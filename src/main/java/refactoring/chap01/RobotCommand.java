c8c2eec4523c4182fbdd4c3ad6ea834aea17ec16
GangDae Lee <enhoomin@naver.com>
2018-03-20 09:22:26 +0900

    robotcommand 클래스 추가

---
+++ src/main/java/refactoring/chap01/RobotCommand.java
@@ -0,0 +1,14 @@
+package refactoring.chap01;
+
+public class RobotCommand {
+	private final String _name;
+
+	public RobotCommand(String _name) {
+		this._name = _name;
+	}
+
+	@Override
+	public String toString() {
+		return String.format("[RobotCommand : %s]", _name);
+	}
+}
