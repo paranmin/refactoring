76483727dd3ad561a5fc9fcd62f1a7e2930ac185
GangDae Lee <enhoomin@naver.com>
2018-03-20 10:22:00 +0900

    제어 플래그 삭제 테스트 실행 추가

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -7,7 +7,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// chapter01();
-		int[] data = {1,9,0,2,8,5,6,3,4,7};
+		int[] data = { 1, 9, 0, 2, 8, 5, 6, 3, 4, 7 };
 		if (FindInt.find(data, 5)) {
 			System.out.println("Found!");
 		} else {
