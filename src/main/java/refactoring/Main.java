0b34272b8d698a78eb450b6d46f40f5a750cebfd
GangDae Lee <enhoomin@naver.com>
2018-03-21 09:50:18 +0900

    어서션 추가 - 미 동작

--- src/main/java/refactoring/Main.java
+++ src/main/java/refactoring/Main.java
@@ -4,17 +4,41 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Random;
 
 import refactoring.chap01.Robot;
 import refactoring.chap02.FindInt;
 import refactoring.chap02.simple.SimpleDatabase;
+import refactoring.chap03.SortSample;
 
 public class Main {
-
+	private static final Random random = new Random(1234);
 	public static void main(String[] args) {
 		// chapter01();
 		// chapter02();
+		// chapter02_simple();
 		
+		execute(10);
+		execute(10);
+		execute(10);
+		execute(10);
+		execute(10);
+	}
+	
+	private static void execute(int length) {
+		int[] data = new int[length];
+		for (int i = 0; i < data.length; i++) {
+			data[i] = random.nextInt(data.length);
+		}
+		
+		SortSample sorter = new SortSample(data);
+		System.out.printf("%6s : %s%n", "BEFORE", sorter);
+		
+		sorter.sort();
+		System.out.printf("%6s : %s%n%n", "AFTER", sorter);
+	}
+
+	private static void chapter02_simple() {
 		try {
 			String fileUrl = Thread.currentThread()
 					.getContextClassLoader()
